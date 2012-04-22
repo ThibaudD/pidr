4c04153b2884f796f8ce9cccfc376a27015298ac
Thibaud <thibaud.de@gmail.com>
2012-04-22 23:43:30 +0200

    net_ssh_multi avec paramètres

--- net_ssh_multi.rb
+++ net_ssh_multi.rb
@@ -6,32 +6,47 @@ $: << "#{ENV['HOME']}/.gem/ruby/1.8/gems/net-ssh-2.3.0/lib"
 
 require 'net/ssh'
 require 'net/ssh/multi'
-#system("cat $OAR_FILE_NODES | uniq | tee machines")
-$tab_host = []
 
-machine = File.open("machines",'r')
+if ARGV.length != 3
+	puts "usage : net_ssh_multi.rb nombre_noeuds nombre_gateway nombre_connexion"
+	Process.exit
+end
+
+$tab_host = []
+machine = File.open("../machines",'r')
 machine.each do |ligne|
         $tab_host << ligne.chomp
 end
 
-$resultat = File.open("_temps_net_ssh","a")
+nb_machines = ARGV[0]
+nb_gateways = ARGV[1]
+window_size = ARGV[2]
+nom = "temps_net_ssh_multi_n_#{nb_machines}_g_#{nb_gateways}_w_#{window_size}.txt"
+$resultat = File.open(nom,"a")
 
-def connect(i)
+def connect(i,gateway,window)
 	tdeb = Time.now
-	Net::SSH::Multi.start(:concurrent_connections =>80) do |session|
+	Net::SSH::Multi.start(:concurrent_connections =>window) do |session|
 		
-		nbGateway = 10
-		session.via $tab_host[0].to_s, 'root', :paranoid => Net::SSH::Verifiers::Null.new
-		for k in nbGateway..i/nbGateway do
-			session.use $tab_host[k].to_s, :user => "root", :paranoid => Net::SSH::Verifiers::Null.new, :via => nil
-		end
-		
-		for j in 2..nbGateway do
-			session.via $tab_host[j-1].to_s, 'root', :paranoid => Net::SSH::Verifiers::Null.new
-			for k in i/nbGateway+1..i*j/nbGateway do
+		nbGateway = gateway
+		if nbGateway != 0 
+			session.via $tab_host[0].to_s, 'root', :paranoid => Net::SSH::Verifiers::Null.new
+			for k in nbGateway..i/nbGateway do
 				session.use $tab_host[k].to_s, :user => "root", :paranoid => Net::SSH::Verifiers::Null.new, :via => nil
 			end
+			
+			for j in 2..nbGateway do
+				session.via $tab_host[j-1].to_s, 'root', :paranoid => Net::SSH::Verifiers::Null.new
+				for k in i/nbGateway+1..i*j/nbGateway do
+					session.use $tab_host[k].to_s, :user => "root", :paranoid => Net::SSH::Verifiers::Null.new, :via => nil
+				end
+			end
+		else
+			for k in 0..i do
+				session.use $tab_host[k].to_s, :user => "root", :paranoid => Net::SSH::Verifiers::Null.new
+			end
 		end
+		
 		session.exec "hostname"
 	end
 	tfin = Time.now
@@ -40,6 +55,6 @@ def connect(i)
 	$resultat.write(tdiff.to_s+"\n")
 end
 
-connect(1000)
+connect(nb_machines.to_i, nb_gateways.to_i,window_size.to_i)
 $resultat.close
 
