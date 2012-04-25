159c7b15b527811ae4d37f6276b2f4ba3cc2287a
Thibaud <thibaud.de@gmail.com>
2012-04-25 16:19:28 +0200

    script traitement

--- average.rb
+++ average.rb
@@ -34,7 +34,27 @@ for file in files do
 	o_file.each_line{ |line|
 			tab << line.to_f
 			}
+	interval = confidence_interval(tab)
+
+	file_name = file.chop.gsub(/_n_[0123456789]*/,'')
+	index = file.chop.index('_n_')
+	num_nodes = ""
+	num__ = 0
+	while( num__ <= 2 ) do
+		if file[index..index]=="_"
+		then	num__ = num__ + 1
+		else
+			num_nodes = "#{num_nodes}#{file[index..index]}"
+		end
+		index = index + 1
+	end
+	num_nodes = num_nodes[1..num_nodes.chop.length]
+	puts num_nodes
+	
+	new_file = File.open("test2/#{file_name}",'a') do |f|
+		f.puts("#{num_nodes} #{average(tab)} #{interval[0]} #{interval[1]}")
+	end
+	
 	p average(tab)
-	p ecart_type(tab)
 	p confidence_interval(tab)
 end
