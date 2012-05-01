bbec1d3fc71a9b561c53a792e944ca76224bbc77
Thibaud <thibaud.de@gmail.com>
2012-05-01 23:35:12 +0200

    mise a jour script

--- script_exp.sh
+++ script_exp.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-for k in `seq 1 30`; do
-	for n in 100 200 400 600 800 1000 ; do
-		for w in 40 100 300 500 ; do
-			for m in 0 1 2 3 ; do
+for k in `seq 1 3`; do
+	for n in '100' '200' '400' '600' '800' '1000' ; do
+		for w in '40' '100' '300' '500' ; do
+			for m in '0' '1' '2' '3' ; do
 				ruby taktuk.rb $n $w $m ;
-			done	
-			for g in 4 10 20 40 ; do
+				sleep 2 ;
+			done ;	
+			for g in '4' '10' '20' '40' ; do
 		 		ruby net_ssh_multi.rb $n $g $w ;
-			done
-		done
-	done
+				sleep 2 ;
+			done ;
+		done ;
+	done ;
 done
 		
