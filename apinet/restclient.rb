e4f9bfdaa8b8a23d80b941537ada9844911ff18d
Thibaud <thibaud.de@gmail.com>
2012-05-22 01:50:12 +0200

    apinet

--- apinet/restclient.rb
+++ apinet/restclient.rb
@@ -14,6 +14,6 @@ oar = RestClient.get("http://127.0.0.1:4567/reserve?nodes=3&walltime=1&s=#{sessi
 oar = JSON.parse oar
 puts oar['nodes']
 
-command = RestClient.get("http://127.0.0.1:4567/lauch?command=hostname&s=#{session[',umber']}")
+command = RestClient.get("http://127.0.0.1:4567/lauch?command=hostname&s=#{session['number']}")
 command = JSON.parse command
 puts command['result_array']
